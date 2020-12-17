165319c19869067da1904ee05b7d1f6e85d0ceaa
yunnasheng <yunnasheng@payeasenet.com>
2020-12-17 10:28:09 +0800

    init project

---
+++ src/test/java/com/lb/springboot/retry/SpringbootRetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lb.springboot.retry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
